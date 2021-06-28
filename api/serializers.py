7f4fafa3f8d075a1825d1b1ace6ce22e4fcb8f43
SOYUNJALIYEV <A.SOYUNJALIYEV@bycn.bouygues-construction.com>
2021-06-28 16:37:48 +0500

    here i go again

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from taksist.models import TaxiProfile, User, TaxiStatus, Category, Status, SaherAra
+from taksist.models import TaxiProfile, User, TaxiStatus, Category, Status, SaherAra, SaherIci, EtrapObalary
 
 class TaxiSerializer(serializers.ModelSerializer):
     username = serializers.SerializerMethodField('get_taksist_name')
@@ -13,14 +13,30 @@ class TaxiSerializer(serializers.ModelSerializer):
         model = TaxiProfile
         fields = ['user_id', 'mobile', 'username', 'cat_id', 'status_id', 'nireden', 'nira']
 
+    
     def get_taksist_name(self, taksist):
         user = User.objects.get(taxiprofile = taksist)
         return user.first_name
 
     def get_nireden(self, taksist):
+        # if self.context.get('cat')==2:
         nireden = SaherAra.objects.get(taxi_id=taksist)
         return nireden.city1.name
+        # elif self.context.get('cat')==3:
+        #     nirede = SaherIci.objects.get(taxi_id=taksist)
+        #     return nirede.city
+        # elif self.context.get('cat')==1:
+        #     nirede = EtrapObalary.objects.get(taxi_id=taksist)
+        #     return nirede.city
+        # else:
+        #     taxi = TaxiProfile.objects.get(user_id=taksist)
+        #     return taxi.user_id
+
 
     def get_nira(self, taksist):
+        # if self.context.get('cat')==2:
         nira = SaherAra.objects.get(taxi_id=taksist)
         return nira.city2.name
+        # else:
+        #     return ''
+        
--- yolagcy/views.py
+++ yolagcy/views.py
@@ -6,14 +6,20 @@ def search_view(request, **kwargs):
     print('inside search view')
     if request.method == 'GET':
         if kwargs.get('nira')=='saherara':
+        
             data = requests.get('http://127.0.0.1:8000/api/saherara/', params=request.GET)
             print(data)
-            print('saherara')
+            print('doing saherara ici in search view')
         elif kwargs.get('nira')=='saherici':
+            print('doing saher ici in search view')
             data = requests.get('http://127.0.0.1:8000/api/saherici/', params=request.GET)
+            print('did saher ici in search view')
         elif kwargs.get('nira')=='etrapobalary':
+            print('doing etrapobalary ici in search view')
             data = requests.get('http://127.0.0.1:8000/api/etrapobalary/', params=request.GET)
+            print('did etrapobalary ici in search view')
         else:
             data = User.objects.all()
     # taxi_drivers=User.objects.all()
+    print('returning render in search view')
     return render(request, 'search.html', {'taxi_drivers':data.json()})
